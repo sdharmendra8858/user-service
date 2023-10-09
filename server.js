385d49e7197852f1aa14834956ea882adf09e78a
Dharmendra verma <sdharmendra8858@gmail.com>
2023-10-09 12:09:58 +0530

    express session setup done

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+"use strict";
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 require("dotenv").config();
-
-const routes = require("./routes");
-
 require("./db/connection");
 
+const { sessionObj, redisClient } = require("./db/redis/redisConnect");
+global.redisClient = redisClient;
+
 const app = express();
+app.use(session(sessionObj));
+
+const routes = require("./routes");
 
 app.use(bodyParser.json());
 
@@ -14,4 +20,4 @@ app.use(routes);
 
 const server = app.listen(process.env.PORT, () => {
     console.log("Listening on server", server.address());
-})
+});
